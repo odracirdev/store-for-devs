51edd630de9f10db305337a53047066381906a5c
Ricardo Cuauro <ricardocuauro93@gmail.com>
2025-07-18 17:16:34 -0400

    fix: add biome ignore comment for unused variables in env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
 	readonly WORDPRESS_CONSUMER_SECRET: string
 }
 
+// biome-ignore lint/correctness/noUnusedVariables: just ignore
 interface ImportMeta {
 	readonly env: ImportMetaEnv
 }
